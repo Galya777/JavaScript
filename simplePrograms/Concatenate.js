279fbffc926da285d54b45b7d72dabe880625bf7
Galya <58034004+Galya777@users.noreply.github.com>
2021-10-02 13:03:13 +0300

    Add files via upload

---
+++ simplePrograms/Concatenate.js
@@ -0,0 +1,8 @@
+function concatenate(input){
+let name=input[0]
+let lastName= input[1]
+let age= input[2]
+let town= input[3]
+console.log('You are '+ name +" "+ lastName+', '+'a '+age+'-years old person from '+town+'.');
+}
+concatenate(["first", "last", "0", "town"])
